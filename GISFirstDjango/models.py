1c1354ecd64b99f1d9f5e0b7132859bd844e7859
Elisa <iselisa978@gmail.com>
2024-01-30 10:33:08 +0800

    adding new classes

--- GISFirstDjango/models.py
+++ GISFirstDjango/models.py
@@ -7,3 +7,11 @@ class Person(models.Model):
 
 class TeachingGroup(models.Model):
     students = models.OneToOneField(Person, null=True, on_delete=models.SET_NULL)
+
+class Student(Person):
+    year = models.IntegerField()
+    tutor_room = models.CharField(max_length=3, blank=True)
+
+class Teacher(Person):
+    tutor_room = models.CharField(max_length=3, blank=True)
+    subject = models.CharField(max_length=30)
