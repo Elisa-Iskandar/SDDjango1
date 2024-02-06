5e5799664e1ceb5e2130a7dac53bd75af31f9c42
Elisa <iselisa978@gmail.com>
2024-02-06 10:33:04 +0800

    adding new classes

--- GISFirstDjango/models.py
+++ GISFirstDjango/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField(blank=False, null=True)
+    age = models.IntegerField(blank=True, null=True)
+
 
 class TeachingGroup(models.Model):
     name = models.CharField(max_length=30, null=True)
